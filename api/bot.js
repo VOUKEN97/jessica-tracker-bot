7207bc5c8b8d8239e617324eaeaa6c04ae0ccad9
VOUKEN97 <4njzpn67v8@privaterelay.appleid.com>
2026-08-25 00:23:50 +0700

    Add files via upload

--- api/bot.js
+++ api/bot.js
@@ -3,12 +3,13 @@ const bot = require('../index');
 module.exports = async (req, res) => {
     try {
         if (req.method === 'POST') {
-            await bot.handleUpdate(req.body, res);
+            await bot.handleUpdate(req.body);
+            res.status(200).send('OK');
         } else {
             res.status(200).send('Bot is running on Vercel!');
         }
     } catch (e) {
-        console.error(e);
-        res.status(500).send('Error');
+        console.error("Webhook Error:", e);
+        res.status(200).send('Error handled'); // Always return 200 to Telegram so it stops retrying
     }
 };
